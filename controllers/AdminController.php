de938cfb5bd9a08c96f3e3e7ee2dd0c8c487e577
Sacha Fauconnier <sacha.faucon@gmail.com>
2026-02-12 09:13:30 +0100

    Initialisation

---
+++ controllers/AdminController.php
@@ -0,0 +1,237 @@
+<?php 
+/**
+ * Contrôleur de la partie admin.
+ */
+ 
+class AdminController {
+
+    /**
+     * Affiche la page d'administration.
+     * @return void
+     */
+    public function showAdmin() : void
+    {
+        // On récupere l'URL
+        $sort = $_GET['sort'] ?? 'date';
+        $order = $_GET['order'] ?? 'asc';
+
+        $nextOrder = ($order === 'asc') ? 'desc' : 'asc';
+
+        // On vérifie que l'utilisateur est connecté.
+        $this->checkIfUserIsConnected();
+
+        // On récupère les articles.
+        $articleManager = new ArticleManager();
+        $articles = $articleManager->getAllArticles();
+
+
+        // On récupère le nombre de commentaire.
+        $commentManager = new CommentManager(); // Assure-toi de passer $this->db si nécessaire
+        $nbComments = [];
+
+        foreach ($articles as $article) {
+            $nbComments[$article->getId()] = $commentManager->countCommentsByArticleId($article->getId());
+
+        }
+          //  On tri les données
+       usort($articles, function($a, $b) use ($sort, $order, $nbComments) {
+
+            $result = 0;
+
+            // On compare selon la colonne
+            if ($sort === 'views') {
+                $result = $a->getViews() <=> $b->getViews();
+            } 
+            elseif ($sort === 'title') {
+                $result = strcmp($a->getTitle(), $b->getTitle());
+            }
+            elseif ($sort === 'date') {
+                $result = $a->getDateCreation() <=> $b->getDateCreation();
+            }
+            elseif ($sort === 'comments') {
+                $result = ($nbComments[$a->getId()] ?? 0) <=> ($nbComments[$b->getId()] ?? 0);
+            }
+
+            // On gére asc / desc
+            return $order === 'asc' ? $result : -$result;
+        });
+
+        // On affiche la page d'administration.
+        $view = new View("Administration");
+        $view->render("admin", [
+            'articles' => $articles,
+            'nbComments' => $nbComments,
+            'nextOrder' => $nextOrder 
+        ]);
+    }
+
+    /**
+     * Vérifie que l'utilisateur est connecté.
+     * @return void
+     */
+    private function checkIfUserIsConnected() : void
+    {
+        // On vérifie que l'utilisateur est connecté.
+        if (!isset($_SESSION['user'])) {
+            Utils::redirect("connectionForm");
+        }
+    }
+
+    /**
+     * Affichage du formulaire de connexion.
+     * @return void
+     */
+    public function displayConnectionForm() : void 
+    {
+        $view = new View("Connexion");
+        $view->render("connectionForm");
+    }
+
+    /**
+     * Connexion de l'utilisateur.
+     * @return void
+     */
+    public function connectUser() : void 
+    {
+        // On récupère les données du formulaire.
+        $login = Utils::request("login");
+        $password = Utils::request("password");
+
+        // On vérifie que les données sont valides.
+        if (empty($login) || empty($password)) {
+            throw new Exception("Tous les champs sont obligatoires. 1");
+        }
+
+        // On vérifie que l'utilisateur existe.
+        $userManager = new UserManager();
+        $user = $userManager->getUserByLogin($login);
+        if (!$user) {
+            throw new Exception("L'utilisateur demandé n'existe pas.");
+        }
+
+        // On vérifie que le mot de passe est correct.
+        if (!password_verify($password, $user->getPassword())) {
+            $hash = password_hash($password, PASSWORD_DEFAULT);
+            throw new Exception("Le mot de passe est incorrect : $hash");
+        }
+
+        // On connecte l'utilisateur.
+        $_SESSION['user'] = $user;
+        $_SESSION['idUser'] = $user->getId();
+
+        // On redirige vers la page d'administration.
+        Utils::redirect("admin");
+    }
+
+    /**
+     * Déconnexion de l'utilisateur.
+     * @return void
+     */
+    public function disconnectUser() : void 
+    {
+        // On déconnecte l'utilisateur.
+        unset($_SESSION['user']);
+
+        // On redirige vers la page d'accueil.
+        Utils::redirect("home");
+    }
+
+    /**
+     * Affichage du formulaire d'ajout d'un article.
+     * @return void
+     */
+    public function showUpdateArticleForm() : void 
+    {
+        $this->checkIfUserIsConnected();
+
+        // On récupère l'id de l'article s'il existe.
+        $id = Utils::request("id", -1);
+
+        // On récupère l'article associé.
+        $articleManager = new ArticleManager();
+        $article = $articleManager->getArticleById($id);
+
+        // Si l'article n'existe pas, on en crée un vide. 
+        if (!$article) {
+            $article = new Article();
+        }
+
+        // On affiche la page de modification de l'article.
+        $view = new View("Edition d'un article");
+        $view->render("updateArticleForm", [
+            'article' => $article
+        ]);
+    }
+
+    /**
+     * Ajout et modification d'un article. 
+     * On sait si un article est ajouté car l'id vaut -1.
+     * @return void
+     */
+    public function updateArticle() : void 
+    {
+        $this->checkIfUserIsConnected();
+
+        // On récupère les données du formulaire.
+        $id = Utils::request("id", -1);
+        $title = Utils::request("title");
+        $content = Utils::request("content");
+
+        // On vérifie que les données sont valides.
+        if (empty($title) || empty($content)) {
+            throw new Exception("Tous les champs sont obligatoires. 2");
+        }
+
+        // On crée l'objet Article.
+        $article = new Article([
+            'id' => $id, // Si l'id vaut -1, l'article sera ajouté. Sinon, il sera modifié.
+            'title' => $title,
+            'content' => $content,
+            'id_user' => $_SESSION['idUser']
+        ]);
+
+        // On ajoute l'article.
+        $articleManager = new ArticleManager();
+        $articleManager->addOrUpdateArticle($article);
+
+        // On redirige vers la page d'administration.
+        Utils::redirect("admin");
+    }
+
+
+    /**
+     * Suppression d'un article.
+     * @return void
+     */
+    public function deleteArticle() : void
+    {
+        $this->checkIfUserIsConnected();
+
+        $id = Utils::request("id", -1);
+
+        // On supprime l'article.
+        $articleManager = new ArticleManager();
+        $articleManager->deleteArticle($id);
+       
+        // On redirige vers la page d'administration.
+        Utils::redirect("admin");
+    }
+
+    public function deleteComment() : void
+    {
+        $this->checkIfUserIsConnected();
+
+        $idComment = Utils::request("id", -1);
+
+        $commentManager = new CommentManager();
+
+        $comment = $commentManager->getCommentById($idComment);
+        if (!$comment) {
+            throw new Exception("Le commentaire n'existe pas.");
+        }
+
+        $commentManager->deleteComment($comment);
+
+        Utils::redirect("showArticle", ['id' => $comment->getIdArticle()]);
+    }
+}
